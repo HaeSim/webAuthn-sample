4133509e56cc5870c65d80100e4dc0a8c932a57c
Dean <cheoljin.an@lotte.net>
2023-09-19 00:10:18 +0900

    fix: 환경변수 수정

--- src/lib/webAuthn/relyingParty.ts
+++ src/lib/webAuthn/relyingParty.ts
@@ -1,9 +1,9 @@
 // Human-readable title for your website
 const rpName = 'SimpleWebAuthn Example';
 // A unique identifier for your website
-const rpID = process.env.VERCEL_URL || 'localhost';
+const rpID = process.env.APP_RPID || 'localhost';
 // The URL at which registrations and authentications should occur
-const origin = process.env.VERCEL_URL || 'http://localhost:3000';
+const origin = process.env.APP_ORIGIN || 'http://localhost:3000';
 
 export const rp = {
   rpName,
